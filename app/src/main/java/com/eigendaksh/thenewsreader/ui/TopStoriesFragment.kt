3d5d0b52a64cb6208daac93aa0f6fbecc247a76a
Ashutosh Purushottam <ashutosh.purushottam@mammoth.la>
2019-07-26 22:14:34 +0530

    Added coroutine

--- app/src/main/java/com/eigendaksh/thenewsreader/ui/TopStoriesFragment.kt
+++ app/src/main/java/com/eigendaksh/thenewsreader/ui/TopStoriesFragment.kt
@@ -2,18 +2,30 @@ package com.eigendaksh.thenewsreader.ui
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.eigendaksh.thenewsreader.R
+import com.eigendaksh.thenewsreader.viewmodel.StoriesViewModel
 
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class TopStoriesFragment : Fragment() {
+
+    private val TAG by lazy { TopStoriesFragment::class.java.simpleName }
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(StoriesViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fetchTopStories()
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +35,16 @@ class TopStoriesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_top_stories, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.observableTopStories?.observe(viewLifecycleOwner, Observer {
+            for (result in it.results) {
+                Log.i(TAG, result.title)
+            }
+        })
+    }
 
+    private fun fetchTopStories() {
+        viewModel.fetchTopStories()
+    }
 }
